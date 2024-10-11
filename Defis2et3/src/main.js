5a8abf4d04f011dc450ffdca6246f1bd86600ba9
corentin-k <c.kervagoret@outlook.fr>
2024-10-11 15:26:44 -0400

    .

--- Defis2et3/src/main.js
+++ Defis2et3/src/main.js
@@ -5,6 +5,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
+
 import './style.css';
 import { createPinia } from 'pinia';
 import router from "./router/router.js";
