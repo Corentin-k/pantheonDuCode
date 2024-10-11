960972d2d315814312490337c313d466c6aee35f
corentin-k <c.kervagoret@outlook.fr>
2024-10-11 15:12:26 -0400

    .

--- Defis2et3/src/main.js
+++ Defis2et3/src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
 import { createPinia } from 'pinia';
 import router from "./router/router.js";
