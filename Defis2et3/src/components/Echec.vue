fc16d6921db4a360efe389da02d81b9946282701
corentin-k <c.kervagoret@outlook.fr>
2024-10-22 18:12:45 -0400

    add win message

--- Defis2et3/src/components/Echec.vue
+++ Defis2et3/src/components/Echec.vue
@@ -1,11 +1,10 @@
 <template>
-  <div>
+  <div v-if="startGame">
     <div class="rowGrid">
       <div class="name"></div>
       <div v-for="letter in letters" :key="letter" class="name">{{ letter }}</div>
     </div>
-
-    <div v-for="(row, rowIndex) in 8" :key="rowIndex" class="rowGrid">
+    <div  v-for="(row, rowIndex) in 8" :key="rowIndex" class="rowGrid">
       <div class="name">{{ 8 - rowIndex }}</div>
       <div v-for="(col, colIndex) in 8" :key="colIndex"
            :class="{
@@ -28,6 +27,11 @@
       <div v-for="letter in letters" :key="letter" class="name">{{ letter }}</div>
     </div>
   </div>
+
+  <div v-else>
+    <h2>{{ endMessage }}</h2>
+    <button @click="restartGame">Rejouer</button>
+  </div>
 </template>
 
 
@@ -36,6 +40,9 @@
 import { reactive, ref } from 'vue';
 import Chess from './chess.js';
 import Piece from "./Piece.js";
+
+const startGame= ref(true)
+const endMessage = ref("");
 
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 const pieces = reactive({
@@ -87,19 +94,18 @@ function handleMove(column, row) {
     selectedPosition.value = position;
   } else {
     chess.move(selectedPosition.value, position);
-    if (chess.echecEtMat()) {
-      alert("Échec et Mat : Le roi a été capturé !");
-    }
-    else {
-      console.log("Le roi n'est pas en échec et mat")
-    }
-    if(chess.pat()){
-      alert("Pat : Le roi n'est pas en échec mais ne peut pas bouger");
-    }
-    else {
-      console.log("Le roi n'est pas en pat")
-    }
     selectedPosition.value = null;
+    if (chess.echecEtMat()) {
+      endMessage.value = "Échec et Mat ! Partie terminée : " + (chess.turn === "white" ? "Les Noirs " : "Les Blancs") + "ont gagné";
+      startGame.value = false;
+      return;
+    }
+
+    if(chess.pat()){
+      endMessage.value = "Pat ! Egalité";
+      startGame.value = false;
+    }
+
   }
 }
 
@@ -125,7 +131,50 @@ function isKingInCheck(column, row) {
   }
   return false;
 }
+function restartGame() {
+  for (const key in pieces) {
+    pieces[key] = null;
+  }
+  Object.assign(pieces, {
+    A1: new Piece('rook', 'black'),
+    A2: new Piece('pawn', 'black'),
+    B1: new Piece('knight', 'black'),
+    B2: new Piece('pawn', 'black'),
+    C1: new Piece('bishop', 'black'),
+    C2: new Piece('pawn', 'black'),
+    D1: new Piece('queen', 'black'),
+    D2: new Piece('pawn', 'black'),
+    E1: new Piece('king', 'black'),
+    E2: new Piece('pawn', 'black'),
+    F1: new Piece('bishop', 'black'),
+    F2: new Piece('pawn', 'black'),
+    G1: new Piece('knight', 'black'),
+    G2: new Piece('pawn', 'black'),
+    H1: new Piece('rook', 'black'),
+    H2: new Piece('pawn', 'black'),
 
+    A7: new Piece('pawn', 'white'),
+    A8: new Piece('rook', 'white'),
+    B7: new Piece('pawn', 'white'),
+    B8: new Piece('knight', 'white'),
+    C7: new Piece('pawn', 'white'),
+    C8: new Piece('bishop', 'white'),
+    D7: new Piece('pawn', 'white'),
+    D8: new Piece('queen', 'white'),
+    E7: new Piece('pawn', 'white'),
+    E8: new Piece('king', 'white'),
+    F7: new Piece('pawn', 'white'),
+    F8: new Piece('bishop', 'white'),
+    G7: new Piece('pawn', 'white'),
+    G8: new Piece('knight', 'white'),
+    H7: new Piece('pawn', 'white'),
+    H8: new Piece('rook', 'white'),
+  });
+
+  startGame.value = true;
+  selectedPosition.value = null;
+  endMessage.value = '';
+}
 </script>
 
 
@@ -169,6 +218,10 @@ function isKingInCheck(column, row) {
 }
 .in-check {
   background-color: red;
+}
+
+h2, p {
+  color: #f9f9f9;
 }
 
 </style>
